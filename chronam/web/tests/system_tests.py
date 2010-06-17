3cec030ed2578c637ab894425138c6e4ac438e5d
Ed Summers <edsu@loc.gov>
2010-06-17 16:35:22 -0400

    upped to django v1.2.1

--- chronam/web/tests/system_tests.py
+++ chronam/web/tests/system_tests.py
@@ -6,7 +6,7 @@ from django.test import TestCase
 class SystemTests(TestCase):
 
     def test_django_version(self):
-        self.assertEqual(django.get_version(), '1.1.1')
+        self.assertEqual(django.get_version(), '1.2.1')
 
     def test_python_version(self):
         self.assertEqual(sys.version_info[0:3], (2,6,5))
