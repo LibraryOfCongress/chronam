3a76475a22f4d355d068b45dc18d459c2a8e66fc
Daniel Krech <eikeon@eikeon.com>
2010-08-17 11:05:16 -0400

    settings.STATIC now know as settings.MEDIA_ROOT to align with django convention

--- conf/settings-staging.py
+++ conf/settings-staging.py
@@ -87,7 +87,7 @@ BIB_STORAGE = '/ndnp/staging/data/bib/data'
 ESSAY_STORAGE = '/ndnp/staging/data/essays'
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
-STATIC = os.path.join(_ROOT, 'static') # TODO: MEDIA_ROOT?
+MEDIA_ROOT = os.path.join(_ROOT, 'static')?
 THUMBNAIL_WIDTH = 200
 
 SOLR = 'http://localhost:8084/solr'
