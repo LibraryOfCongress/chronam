aad0d2b326b94f97dc6ba15bf5d4b8dd5e46d053
Daniel Krech <dkre@loc.gov>
2010-08-23 11:19:55 -0400

    added files to support new projects/apps structure

---
+++ apps/chronam-core/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import os
+
+
+setup(name='chronam-core',
+      version="0.0.0",
+      description='',
+      author='',
+      author_email='',
+      url='',
+      download_url='',
+      packages=['chronam', 'chronam.core', 'chronam.utils'],
+      )
---
+++ apps/chronam-web/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import os
+
+
+setup(name='chronam-web',
+      version="0.0.0",
+      description='',
+      author='',
+      author_email='',
+      url='',
+      download_url='',
+      packages=['chronam', 'chronam.web'],
+      )
