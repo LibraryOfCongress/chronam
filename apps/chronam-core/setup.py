94d2b18b9496554ebcca59d962705c1ae1f734b6
Daniel Krech <eikeon@eikeon.com>
2010-08-31 14:35:53 -0400

    updated to use rfc3339 as an external dependency

--- apps/chronam-core/setup.py
+++ apps/chronam-core/setup.py
@@ -10,5 +10,5 @@ setup(name='chronam-core',
       url='',
       download_url='',
       packages=['chronam', 'chronam.core', 'chronam.utils'],
-      requires=["feedparser"]
+      requires=['feedparser', 'rfc3339']
       )
