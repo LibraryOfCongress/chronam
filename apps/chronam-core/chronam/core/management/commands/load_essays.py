571e48b3dc48f0df05b41850dbb86993bdb0a3a6
Ed Summers <edsu@loc.gov>
2010-12-13 22:20:05 -0500

    use the filename to determine the expected uri of the essay (not the graph) since it is more likely to be correct ; also fixed bug that was preventing purged essays to trigger title reindex in solr

--- apps/chronam-core/chronam/core/management/commands/load_essays.py
+++ apps/chronam-core/chronam/core/management/commands/load_essays.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
@@ -8,10 +9,19 @@ from chronam.core.management.commands import configure_logging
 
 
 configure_logging("load_essays.config", 'load_essays.log')
+log = logging.getLogger()
 
 class Command(BaseCommand):
     help = "load all the essays"
 
     def handle(self, *args, **options):
-        for essay_file in os.listdir(settings.ESSAY_STORAGE):
-            load_essay(essay_file)
+        essay_files = os.listdir(settings.ESSAY_STORAGE)
+        essay_files.sort()
+
+        for essay_file in essay_files:
+            if "template.html" in essay_file:
+                continue
+            try:
+                load_essay(essay_file)
+            except Exception, e:
+                log.error("unable to load essay %s: %s" % (essay_file, e))
