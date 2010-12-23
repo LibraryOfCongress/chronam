823b6c25a82081d910b53b4b75d625262a28c498
Ed Summers <ehs@pobox.com>
2010-12-22 21:51:38 -0500

    added google news markup to the sitemap. fixes #679

--- apps/chronam-core/chronam/core/management/commands/release.py
+++ apps/chronam-core/chronam/core/management/commands/release.py
@@ -8,17 +8,18 @@ import os
 import re
 import logging
 
-from optparse import make_option
 from time import mktime
 from datetime import datetime
+from optparse import make_option
 
 import feedparser
 from rfc3339 import rfc3339
 
 from django.conf import settings
-from django.core.management.base import BaseCommand
-from django.core.paginator import Paginator
 from django.db import reset_queries
+from django.core.paginator import Paginator
+from django.core.management.base import BaseCommand
+from django.template.defaultfilters import force_escape
 
 from chronam.core.management.commands import configure_logging
 from chronam.core import models as m
@@ -97,7 +98,7 @@ def write_sitemaps():
     url_count = 0
     sitemap_file = None
 
-    for loc, last_mod in sitemap_urls():
+    for loc, last_mod, newspaper, pub_date, title in sitemap_urls():
 
         # if we've maxed out the number of urls per sitemap 
         # close out the one we have open and open a new one
@@ -108,13 +109,15 @@ def write_sitemaps():
                 sitemap.close()
             sitemap_file = 'sitemap-%05d.xml' % page_count
             _logger.info("writing %s" % sitemap_file)
-            sitemap = open(
-                os.path.join(settings.DOCUMENT_ROOT, sitemap_file), 'w')
-            sitemap.write('<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
+            sitemap = open(os.path.join(settings.DOCUMENT_ROOT, sitemap_file), 'w')
+            sitemap.write('<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">\n')
             sitemap_index.write('<sitemap><loc>http://chroniclingamerica.loc.gov/%s</loc></sitemap>\n' % sitemap_file)
     
         # add a url to the sitemap
-        sitemap.write("<url><loc>http://chroniclingamerica.loc.gov%s</loc><lastmod>%s</lastmod></url>\n" % (loc, rfc3339(last_mod)))
+        if newspaper and pub_date and title:
+            sitemap.write("<url><loc>http://chroniclingamerica.loc.gov%s</loc><lastmod>%s</lastmod><news:publication><news:name>%s</news:name><news:language>en</news:language></news:publication><news:publication_date>%s</news:publication_date><news:title>%s</news:title></url>\n" % (loc, rfc3339(last_mod), force_escape(newspaper), rfc3339(pub_date), force_escape(title)))
+        else:
+            sitemap.write("<url><loc>http://chroniclingamerica.loc.gov%s</loc><lastmod>%s</lastmod></url>\n" % (loc, rfc3339(last_mod)))
         url_count += 1
 
         # necessary to avoid memory bloat when settings.DEBUG = True
@@ -133,18 +136,19 @@ def sitemap_urls():
     titles, and their respective modified time as a tuple.
     """
     for batch in m.Batch.objects.all():
-        yield batch.url, batch.released
-        yield rdf_uri(batch), batch.released
+        yield batch.url, batch.released, None, None, None
+        yield rdf_uri(batch), batch.released, None, None, None
         for issue in batch.issues.all():
-            yield issue.url, batch.released
-            yield rdf_uri(issue), batch.released
+            yield issue.url, batch.released, None, None, None
+            yield rdf_uri(issue), batch.released, None, None, None
             for page in issue.pages.all():
-                yield page.url, batch.released
-                yield rdf_uri(page), batch.released
+                yield page.url, batch.released, page.issue.title.name, \
+                      page.issue.date_issued, unicode(page)
+                yield rdf_uri(page), batch.released, None, None, None
 
     paginator = Paginator(m.Title.objects.all(), 10000)
     for page_num in range(1, paginator.num_pages + 1):
         page = paginator.page(page_num)
         for title in page.object_list:
-            yield title.url, title.created
+            yield title.url, title.created, None, None, None
 
