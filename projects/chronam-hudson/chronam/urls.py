db3b32580bf884d88553bb94a7514800dac19a97
Daniel Krech <eikeon@eikeon.com>
2011-01-04 11:43:30 -0500

    removed bits that have been split out into their own repos; added INSTALL and conf bits.

--- projects/chronam-hudson/chronam/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import patterns, url, include
-
-
-urlpatterns = patterns(
-    '',
-
-    (r'^', include('chronam.web.urls')),
-
-)
--- projects/chronam-public1/chronam/settings_rdc.py
+++
@@ -1,5 +0,0 @@
-from chronam.settings_global import *
-
-STORAGE = '/vol/ndnp/chronam/batches'
-BIB_STORAGE = '/vol/ndnp/chronam/bib/data'
-ESSAY_STORAGE = '/vol/ndnp/chronam/ndnp-essays/essays'
