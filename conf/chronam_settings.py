8b27cec07e532dcc523fd9fce59a1e61d07dbdd7
Ed Summers <edsu@loc.gov>
2011-01-06 16:25:53 -0500

    chronam_loc_settings are the default for this public facing view of chronam

--- conf/chronam_settings.py
+++ conf/chronam_settings.py
@@ -1,7 +1,7 @@
 import os
 
 from chronam_core_settings import *
-from chronam.core.settings_default import *
+from chronam_loc.settings_default import *
 
 
 DEBUG = True
@@ -10,3 +10,5 @@ TEMPLATE_DEBUG = True
 OMNITURE_SCRIPT = "http://www.loc.gov:8081/global/s_code.js"
 DEFAULT_TTL_SECONDS = 1
 PAGE_IMAGE_TTL_SECONDS = 1
+
+MEMORIOUS_REPOSITORIES = {"default": "/ndnp/chronam/data/memorious"}
