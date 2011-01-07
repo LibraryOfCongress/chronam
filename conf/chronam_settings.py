cfe6757d5e1d44a6b7d26ad2e18803b57e9f8d7a
Daniel Krech <dkre@loc.gov>
2011-01-07 15:42:37 -0500

    more /ndnp -> /opt missed in d8bbc30d1b68

--- conf/chronam_settings.py
+++ conf/chronam_settings.py
@@ -11,4 +11,4 @@ OMNITURE_SCRIPT = "http://www.loc.gov:8081/global/s_code.js"
 DEFAULT_TTL_SECONDS = 1
 PAGE_IMAGE_TTL_SECONDS = 1
 
-MEMORIOUS_REPOSITORIES = {"default": "/ndnp/chronam/data/memorious"}
+MEMORIOUS_REPOSITORIES = {"default": "/opt/chronam/data/memorious"}
