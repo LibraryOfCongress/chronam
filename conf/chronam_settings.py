40b415f2c66100cd64ab42e5c03343c8975ac541
Ed Summers <edsu@loc.gov>
2011-02-01 18:08:33 -0500

    added mod_cache options, and turned off debug

--- conf/chronam_settings.py
+++ conf/chronam_settings.py
@@ -10,5 +10,6 @@ TEMPLATE_DEBUG = True
 OMNITURE_SCRIPT = "http://www.loc.gov:8081/global/s_code.js"
 DEFAULT_TTL_SECONDS = 1
 PAGE_IMAGE_TTL_SECONDS = 1
+STORAGE = "/opt/chronam/data/batches/"
 
 MEMORIOUS_REPOSITORIES = {"default": "/opt/chronam/data/memorious"}
