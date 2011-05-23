b11f9ecafb4451937c441ecf4fff40b7d2cf45e7
Risa Ohara <rioh@loc.gov>
2011-05-23 09:15:18 -0400

    added feed TTL

--- conf/chronam_settings.py
+++ conf/chronam_settings.py
@@ -10,6 +10,7 @@ TEMPLATE_DEBUG = False
 OMNITURE_SCRIPT = "http://www.loc.gov:8081/global/s_code.js"
 DEFAULT_TTL_SECONDS = 60 * 60 * 24
 PAGE_IMAGE_TTL_SECONDS = 60 * 60 * 24
+FEED_TTL_SECONDS = 60 * 60 * 24 * 7
 STORAGE = "/opt/chronam/data/batches/"
 
 MEMORIOUS_REPOSITORIES = {"default": "/opt/chronam/data/memorious"}
