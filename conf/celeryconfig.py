13cf4fa657d2386a95b421e8611dffaa281b5f43
Ed Summers <ehs@pobox.com>
2011-10-28 15:14:20 -0400

    celery config for cts integration

---
+++ conf/celeryconfig.py
@@ -0,0 +1,28 @@
+import os
+import logging
+import datetime
+
+from chronam_core_settings import *
+from chronam_loc.settings_default import *
+
+APP_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+BROKER_BACKEND = "djkombu.transport.DatabaseTransport"
+
+INSTALLED_APPS += ("djkombu", "djcelery" )
+
+CELERY_IMPORTS = ("chronam.core.tasks",)
+CELERYD_LOG_FILE = os.path.join(APP_DIR, "logs", "celery.log")
+CELERYD_LOG_LEVEL = logging.INFO
+CELERYD_CONCURRENCY = 1
+
+CELERYBEAT_SCHEDULE = {
+    "poll_cts": {
+        "task": "chronam.core.tasks.poll_cts",
+        "schedule": datetime.timedelta(seconds=30),
+        "args": ()
+    }
+}
+
+CELERYBEAT_LOG_FILE = os.path.join(APP_DIR, "logs", "celerybeat.log")
+CELERYBEAT_LOG_LEVEL = logging.INFO
