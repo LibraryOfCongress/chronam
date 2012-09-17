456ad3fa80af94c57030c09ca51051f9654d0ea7
Risa Ohara <rioh@loc.gov>
2012-09-17 17:26:59 -0400

    refs #1258: removed english stats for language reports page

--- core/views/reports.py
+++ core/views/reports.py
@@ -519,13 +519,14 @@ def languages(request):
 def language_batches(request, language, page_number=1):
     language_name = models.Language.objects.get(code=language).name
     page_title = 'Batches with %s text' % (language_name)
-    batches = models.LanguageText.objects.filter(language__code=language).values('ocr__page__issue__batch').annotate(count=Count('ocr__page__issue__batch'))
-    paginator = Paginator(batches, 25)
-    try:
-        page = paginator.page(page_number)
-    except InvalidPage:
-        page = paginator.page(1)
-    page_range_short = list(_page_range_short(paginator, page))
+    if language != "eng":
+        batches = models.Batch.objects.filter(issues__pages__ocr__language_texts__language__code=language).values('name').annotate(count=Count('name'))
+        paginator = Paginator(batches, 25)
+        try:
+            page = paginator.page(page_number)
+        except InvalidPage:
+            page = paginator.page(1)
+        page_range_short = list(_page_range_short(paginator, page))
     return render_to_response('reports/language_batches.html', dictionary=locals(),
                               context_instance=RequestContext(request))
 
@@ -534,13 +535,14 @@ def language_batches(request, language, page_number=1):
 def language_titles(request, language, page_number=1):
     language_name = models.Language.objects.get(code=language).name
     page_title = 'Titles with %s text' % (language_name)
-    titles = models.LanguageText.objects.filter(language__code=language).values('ocr__page__issue__title__lccn', 'ocr__page__issue__batch__name').annotate(count=Count('ocr__page__issue__title__lccn'))
-    paginator = Paginator(titles, 25)
-    try:
-        page = paginator.page(page_number)
-    except InvalidPage:
-        page = paginator.page(1)
-    page_range_short = list(_page_range_short(paginator, page))
+    if language != "eng":
+        titles = models.Title.objects.filter(issues__pages__ocr__language_texts__language__code=language).values('lccn', 'issues__batch__name').annotate(count=Count('lccn'))
+        paginator = Paginator(titles, 25)
+        try:
+            page = paginator.page(page_number)
+        except InvalidPage:
+            page = paginator.page(1)
+        page_range_short = list(_page_range_short(paginator, page))
     return render_to_response('reports/language_titles.html', dictionary=locals(),
                               context_instance=RequestContext(request))
 
@@ -549,18 +551,19 @@ def language_titles(request, language, page_number=1):
 def language_pages(request, language, batch, title=None, page_number=1):
     language_name = models.Language.objects.get(code=language).name
     page_title = 'Pages with %s text' % (language_name)
-    if title:
-        pages = models.Page.objects.filter(ocr__language_texts__language__code=language, issue__title__lccn=title).values('reel__number', 'issue__date_issued', 'issue__title__lccn', 'issue__edition', 'sequence', ).order_by('reel__number', 'issue__date_issued', 'sequence')
-        path = 'reports/language_title_pages.html' 
-    else:
-        pages = models.Page.objects.filter(ocr__language_texts__language__code=language, issue__batch__name=batch).values('reel__number', 'issue__date_issued', 'issue__title__lccn', 'issue__edition', 'sequence', ).order_by('reel__number', 'issue__title__lccn', 'issue__date_issued', 'sequence')
-        path = 'reports/language_batch_pages.html' 
-    paginator = Paginator(pages, 25)
-    try:
-        page = paginator.page(page_number)
-    except InvalidPage:
-        page = paginator.page(1)
-    page_range_short = list(_page_range_short(paginator, page))
+    path = 'reports/language_title_pages.html' 
+    if language != 'eng':
+        if title:
+            pages = models.Page.objects.filter(ocr__language_texts__language__code=language, issue__title__lccn=title).values('reel__number', 'issue__date_issued', 'issue__title__lccn', 'issue__edition', 'sequence', ).order_by('reel__number', 'issue__date_issued', 'sequence')
+        else:
+            pages = models.Page.objects.filter(ocr__language_texts__language__code=language, issue__batch__name=batch).values('reel__number', 'issue__date_issued', 'issue__title__lccn', 'issue__edition', 'sequence', ).order_by('reel__number', 'issue__title__lccn', 'issue__date_issued', 'sequence')
+            path = 'reports/language_batch_pages.html' 
+        paginator = Paginator(pages, 25)
+        try:
+            page = paginator.page(page_number)
+        except InvalidPage:
+            page = paginator.page(1)
+        page_range_short = list(_page_range_short(paginator, page))
     return render_to_response(path, dictionary=locals(),
                               context_instance=RequestContext(request))
 
