9617235b48a671df07404010f53244944b06137e
Ed Summers <edsu@loc.gov>
2012-09-21 10:29:27 -0400

    added django.utils.strftime that makes it easy to use django.utils.datetime_safe to format a date and applied to relevan json methods in chronam.core.models. fixes #1261

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,2 +1,8 @@
 # __init__.py
 
+from django.utils import datetime_safe
+
+def strftime(d, fmt):
+    """works with datetimes with years less than 1900
+    """
+    return datetime_safe.new_datetime(d).strftime(fmt)
