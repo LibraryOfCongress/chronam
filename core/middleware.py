7f2c57b1b92a776d15ef2d5be0e0bbab560fce84
Daniel Krech <dkre@loc.gov>
2012-11-07 14:43:15 -0500

    added chronam's version of the fail whale; closes #1164

--- core/middleware.py
+++ core/middleware.py
@@ -13,5 +13,23 @@ class TooBusyMiddleware(object):
     def process_request(self, request):
         one, five, fifteen = os.getloadavg()
         if one > settings.TOO_BUSY_LOAD_AVERAGE:
-            return HttpResponseServiceUnavailable()
+            return HttpResponseServiceUnavailable("""
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Server Too Busy - Chronicling America - Chronicling America (The Library of Congress)</title> 
+  <style></style>
+</head>
+<body>
+     <article>
+	  <h1>Server Too Busy</h1>
+	   <div>
+            <p>The Chronicling America server is currently too busy to serve your request. Please try your request again shortly.</p>
+            <p><a href="http://www.loc.gov/pictures/resource/ppmsc.01752/"><img src="http://lcweb2.loc.gov/service/pnp/ppmsc/01700/01752r.jpg"/></a></p>
+	   </div>
+     </article>
+</body>
+</html>
+""")
         return None
