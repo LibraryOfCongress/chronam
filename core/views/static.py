2a31f3a20e73395f4c50021983d24ba9c3dbc680
Daniel Krech <dkre@loc.gov>
2012-12-11 11:26:53 -0500

    added crumbs for static pages

--- core/views/static.py
+++ core/views/static.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core import urlresolvers
 from chronam.core.decorator import cache_page
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -6,17 +7,32 @@ from django.template import RequestContext
 @cache_page(settings.DEFAULT_TTL_SECONDS)
 def about(request):
     page_title = "About Chronicling America"
+    crumbs = [
+        {'label':'About',
+         'href': urlresolvers.reverse('chronam_about'),
+         'active': True},
+    ]
     return render_to_response('about.html', dictionary=locals(),
                               context_instance=RequestContext(request))
 
 @cache_page(settings.DEFAULT_TTL_SECONDS)
 def about_api(request):
     page_title = "About the Site and API"
+    crumbs = [
+        {'label':'About API',
+         'href': urlresolvers.reverse('chronam_about_api'),
+         'active': True},
+    ]
     return render_to_response('about_api.html', dictionary=locals(),
                               context_instance=RequestContext(request))
 
 @cache_page(settings.DEFAULT_TTL_SECONDS)
 def help(request):
     page_title = "Help"
+    crumbs = [
+        {'label':'Help',
+         'href': urlresolvers.reverse('chronam_help'),
+         'active': True},
+    ]
     return render_to_response('help.html', dictionary=locals(),
                               context_instance=RequestContext(request))
