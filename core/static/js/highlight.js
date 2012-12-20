2e932c59ca330e04663f832be510996a7905f4d7
Daniel Krech <dkre@loc.gov>
2012-12-20 14:18:16 -0500

    switch highlight coordinates to percentage so they continue to overlay the image when it's resized; removed padding and offset from highlight (so they can be added as desired from CSS)

--- core/static/js/highlight.js
+++ core/static/js/highlight.js
@@ -1,11 +1,5 @@
 (function($) {
 
-    var padding = 5;
-
-    var offset = 0; /* Think the 'a' tag around the thumbnail has a border
-		       that shifts the thumbnail down and to the right a couple pixels... so
-		       we do the same with the highlights */
-
     function add_highlights(image) {
         if (image.data('highlighted')==true) {
             return
@@ -18,22 +12,22 @@
             var id = image.data('id');
             var words = image.data('words');
             $.getJSON(script_name+id+'coordinates/', function(all_coordinates) {
-                image.wrap('<div class="highlight_words" style="display: inline-block; position: relative; margin: 0px; padding: 0px;" />');
+                image.wrap('<div class="highlight_words" style="display: inline-block; position: relative; margin: 0px; padding: 0px; height: auto; width: auto;" />');
                 var div = image.parents("div.highlight_words")
 		div.wrap('<div style="text-align: center" />')
 
-                var vScale = height / all_coordinates["height"];
-                var hScale = width / all_coordinates["width"];
+                var vScale = 100 / all_coordinates["height"];
+                var hScale = 100 / all_coordinates["width"];
                 $.each(words.split(" "), function(index, word) {
                     var boxes = [];
                     var coordinates = all_coordinates["coords"][word];
                     for (k in coordinates) {
                         var v = coordinates[k];
                         div.append("<div style='position: absolute; " +
-    "TOP: " +  (v[1] * vScale - padding/2.0 + offset) + 'px; ' +
-    "LEFT: " +       (v[0] * hScale - padding/2.0 + offset) + 'px; ' +
-    "HEIGHT: " +     (v[3] * vScale + padding) + 'px; ' +
-    "WIDTH: " +      (v[2] * hScale + padding) + "px;'/>");
+    "TOP: " +  (v[1] * vScale) + '%; ' +
+    "LEFT: " +       (v[0] * hScale) + '%; ' +
+    "HEIGHT: " +     (v[3] * vScale) + '%; ' +
+    "WIDTH: " +      (v[2] * hScale) + "%;'/>");
 
                     }
                 });
