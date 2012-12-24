a37970a91b2b41b233768df9c6505cd486a830c5
Jackie Kazil <jkazil@loc.gov>
2012-12-24 18:15:13 -0500

    Updating holdings test to reflect updated changes.

--- core/tests/holding_loader_tests.py
+++ core/tests/holding_loader_tests.py
@@ -12,25 +12,29 @@ class HoldingLoaderTests(TestCase):
     fixtures = ['countries.json', 'languages.json', 'institutions.json']
 
     def test_holdings(self):
-        # the combined title/holdings data
-        marcxml = os.path.join(os.path.dirname(chronam.core.__file__), 
-            'test-data', 'bib.xml')
+        # title data
+        titlexml = os.path.join(os.path.dirname(chronam.core.__file__), 
+            'test-data', 'title.xml')
+
+        # holdings data
+        holdingsxml = os.path.join(os.path.dirname(chronam.core.__file__), 
+            'test-data', 'holdings.xml')
 
         # first need to load the titles so we can link against them
         title_loader = TitleLoader()
-        title_loader.load_file(marcxml)
+        title_loader.load_file(titlexml)
 
         # now load the holdings from the same file
         holding_loader = HoldingLoader()
-        holding_loader.load_file(marcxml)
-
+        holding_loader.load_file(holdingsxml)
+        
         # fetch the title and see that holdings are attached
-        #t = Title.objects.get(lccn='sn83030846')
-        #holdings = list(t.holdings.all())
-        #self.assertEqual(len(holdings), 5)
-        #h = holdings[1]
-        #self.assertEqual(h.institution.name, 'Enfield Free Pub Libr')
-        #self.assertEqual(h.type, 'Original')
-        #self.assertEqual(h.description, '<1978:1:4, 3:2, 9:28-12:21> <1979:1:11, 3:29, 4:12-8:9>')
-        #self.assertEqual(h.description_as_list(), ['<1978:1:4, 3:2, 9:28-12:21>', '<1979:1:11, 3:29, 4:12-8:9>'])
-        #self.assertEqual(str(h.last_updated), '10/1991')
+        t = Title.objects.get(lccn='sn83030846')
+        holdings = list(t.holdings.all())
+        self.assertEqual(len(holdings), 10)
+        h = holdings[1]
+        self.assertEqual(h.institution.name, 'Colgate Univ')
+        self.assertEqual(h.type, 'Original')
+        self.assertEqual(h.description, 'd 3:2, 9:28-12:21> <1979:1:11, 3:29, 4:12-8:9>')
+        self.assertEqual(h.description_as_list(), [u'<1876:5:18>', u'<1884:1:10>', u'<1885:9:3>'])
+        self.assertEqual(str(h.last_updated), '01/1992')
