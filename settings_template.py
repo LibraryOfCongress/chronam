95626e321e310f3659486631078c11d71607c55a
Risa Ohara <rioh@loc.gov>
2013-03-18 13:41:37 -0400

    changed timeout settings for django cache to 2 months

--- settings_template.py
+++ settings_template.py
@@ -97,7 +97,7 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
         'LOCATION': '/var/tmp/django_cache',
-        'TIMEOUT': 100,
+        'TIMEOUT': 4838400,  # 2 months
     }
 }
 
