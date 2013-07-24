99e96a3f2b76ce80b789a6dad4ef793168f82313
Keshav Magge <prasannakeshav@gmail.com>
2013-07-24 13:36:59 -0400

    fixes #61: Default date range to 1836-1922 on all pages

--- core/views/home.py
+++ core/views/home.py
@@ -63,7 +63,7 @@ def frontpages(request, date):
 
 def tabs(request, date=None):
     params = request.GET if request.GET else None
-    form = forms.SearchPagesForm(params)
+    form = forms.SearchPagesForm()
     adv_form = forms.AdvSearchPagesForm(params)
     context = RequestContext(request, {'search_form': form,
                                        'adv_search_form': adv_form})
