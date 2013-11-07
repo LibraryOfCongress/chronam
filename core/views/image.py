87d0216fdc88ede6a35fed222d551b1f97d94a8f
Keshav Magge <prasannakeshav@gmail.com>
2013-11-07 16:06:46 -0500

    Add hook to check coordinates presense for punctuation marks

--- core/views/image.py
+++ core/views/image.py
@@ -2,6 +2,10 @@ import logging
 import os.path
 import urlparse
 import urllib2
+import json
+import gzip
+import re
+
 from cStringIO import StringIO
 
 from django.conf import settings
@@ -10,7 +14,6 @@ from django.http import Http404, HttpResponse, HttpResponseServerError
 from chronam.core import models
 from chronam.core.utils.utils import get_page
 from chronam.core.decorator import cors
-
 
 LOGGER = logging.getLogger(__name__)
 
@@ -132,14 +135,19 @@ def image_tile(request, path, width, height, x1, y1, x2, y2):
 def coordinates(request, lccn, date, edition, sequence, words=None):
     url_parts = dict(lccn=lccn, date=date, edition=edition, sequence=sequence)
     try:
-        f = open(models.coordinates_path(url_parts))
+        file_data = gzip.open(models.coordinates_path(url_parts), 'rb')
     except IOError:
         return HttpResponse()
-    data = f.read()
+
+    data = json.load(file_data)
+
+    non_lexemes = re.compile('''^[^a-zA-Z0-9]+|[^a-zA-Z0-9]+$|'s$''')
+    return_coords = data.copy()
+    # reset coords to {} and build afresh, getting rid of unwanted punctuations
+    return_coords['coords'] = {}
+    for key in data.get('coords'):
+        return_coords['coords'][re.sub(non_lexemes, '', key)] = data['coords'][key]
 
     r = HttpResponse(mimetype='application/json')
-    r['Content-Encoding'] = 'gzip'
-    r['Content-Length'] = len(data)
-    r.write(data)
-    f.close()
+    r.write(return_coords)
     return r
