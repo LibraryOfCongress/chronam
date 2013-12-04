53df7948b5ae8b24d45724f6330c946c2f22ca2b
Keshav Magge <prasannakeshav@gmail.com>
2013-12-04 14:52:39 -0500

    Update settings_template.py
    add ALLOWED_HOSTS setting

--- settings_template.py
+++ settings_template.py
@@ -131,3 +131,7 @@ TEMP_TEST_DATA = os.path.join(STORAGE, "temp_test_data")
 
 
 BASE_CRUMBS = [{'label':'Home', 'href': '/'}]
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['*']
