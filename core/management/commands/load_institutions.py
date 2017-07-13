f416d9dc4f4c1439a5d38913ccb21940f3b45220
John Scancella <jsca@loc.gov>
2017-07-13 08:18:26 -0400

    refs #141 updated load_institutions command to make it more useful

--- core/management/commands/load_institutions.py
+++ core/management/commands/load_institutions.py
@@ -7,15 +7,11 @@ from django.core.management.base import BaseCommand
 from chronam.core.management.commands import configure_logging
 from chronam.core.models import Institution
 
-configure_logging("load_intitutions_logging.config", 
-                  "load_institutions_%s.log" % os.getpid())
+configure_logging("load_intitutions_logging.config", "load_institutions.log")
 
 """
-Simple command to load institution data obtained from the MySQL database 
-running in the MARC Standards office.
-
-"oid","orgName","altname1","altname2","altname3","altname4","orgCode","lowercode","isilCode","obsoleteOrgCode","createDate","modifiedDate","address1","address2","address3","city","stateID","zip","countryID","ID","cname","prefix","searchable"
-22035,"3Com Corporation Technical Library","","","","","CStcTCC","cstctcc","US-CStcTCC","","1995-10-19 00:00:00","1995-10-19 00:00:00","5400 Bayfront Plaza","","","Santa Clara",5,"95052",210,210,"United States","US","yes"
+Loads the institutions based on a CSV file in the form of:
+<code>, <name>, <address1>, <address2>, <city>, <state>, <zip>
 """
 
 class Command(BaseCommand):
@@ -24,16 +20,14 @@ class Command(BaseCommand):
 
     def handle(self, csv_file, *args, **options):
         for row in unicode_csv_reader(codecs.open(csv_file, encoding='utf-8')): 
-            if row[20] != 'United States':
-                continue
             i = Institution()
-            i.code = row[7].upper()
+            i.code = row[0].upper()
             i.name = row[1]
-            i.address1 = row[12]
-            i.address2 = row[13]
-            i.city = row[15]
-            i.state = "MD" # XXX: use real state! row[16]
-            i.zip = row[17]
+            i.address1 = row[2]
+            i.address2 = row[3]
+            i.city = row[4]
+            i.state = row[5]
+            i.zip = row[6]
             i.save()
 
 # some hoops to get csv reader to emit unicode
