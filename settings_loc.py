f339b15cebb36b04db4ecbf5e7f466faa771a197
John Scancella <jsca@loc.gov>
2017-11-03 12:40:00 -0400

    add overrides for settings_loc.py for production

--- settings_loc.py
+++ settings_loc.py
@@ -17,3 +17,6 @@ SEARCH_RESULTS_PER_PAGE = 20
 OMNITURE_SCRIPT = "//cdn.loc.gov/js/global/metrics/sc/s_code.js"
 SHARETOOL_URL = "//cdn.loc.gov/sites/chronicling-america.js"
 
+DEBUG = FALSE
+
+IS_PRODUCTION = True
