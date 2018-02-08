edfee848fc305716dd67d4c8d33a72aef92bd3ab
John Scancella <jsca@loc.gov>
2018-02-08 12:29:43 -0500

    add command to just dump a single batch's ocr

---
+++ core/management/commands/dump_batch_ocr.py
@@ -0,0 +1,22 @@
+import logging
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from chronam.core.models import Batch, OcrDump
+
+LOGGER = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    help = "dump ocr for a single batch"
+    args = '<batch name>'
+
+    def handle(self, batch_name, *args, **options):
+        if not os.path.isdir(settings.OCR_DUMP_STORAGE):
+            os.makedirs(settings.OCR_DUMP_STORAGE)
+
+        batch = Batch.objects.get(name=batch_name)
+        LOGGER.info("starting to dump ocr for %s", batch)
+        dump = OcrDump.new_from_batch(batch)
+        LOGGER.info("created ocr dump %s for %s", dump, batch)
