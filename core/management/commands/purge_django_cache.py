fe931aba5317071e10f277949bbb827c25822153
John Scancella <jsca@loc.gov>
2018-04-10 09:38:35 -0400

    added option to purge command to clear everything from django cache

--- core/management/commands/purge_django_cache.py
+++ core/management/commands/purge_django_cache.py
@@ -1,28 +1,40 @@
 import logging
 
+from optparse import make_option
+
 from django.core.cache import cache
 from django.core.management.base import BaseCommand, CommandError
 
 LOGGER = logging.getLogger(__name__)
 
 class Command(BaseCommand):
+    purge_all = make_option('--all',
+                          action='store_true',
+                          dest='purge_all',
+                          default=False,
+                          help='Purge everything from the django cache')
+
+    option_list = BaseCommand.option_list + (purge_all,)
     help = "Purge the django cache after ingest/purge of a batch"
 
     def handle(self, *args, **options):
 
         try:
-            # delete the total pages count
-            LOGGER.info('removing newspaper_info from cache')
-            cache.delete('newspaper_info')
+            if options['purge_all']:
+                LOGGER.info("clearing the whole django cache")
+                cache.clear()
+            else:
+                # delete the total pages count
+                LOGGER.info('removing newspaper_info from cache')
+                cache.delete('newspaper_info')
 
-            # delete the advanced search title list
-            LOGGER.info('removing titles_states from cache')
-            cache.delete('titles_states')
+                # delete the advanced search title list
+                LOGGER.info('removing titles_states from cache')
+                cache.delete('titles_states')
 	
-	    # delete the fulltext date range
-            LOGGER.info('removing fulltext_range')
-            cache.delete('fulltext_range')
-
+	        # delete the fulltext date range
+                LOGGER.info('removing fulltext_range')
+                cache.delete('fulltext_range')
 
         except Exception, e:
             LOGGER.exception(e)
