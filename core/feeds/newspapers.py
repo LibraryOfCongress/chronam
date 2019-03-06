1374d72a586bf8c7214851cac0da11821aab600b
Chris Adams <cadams@loc.gov>
2019-03-06 14:41:44 -0500

    Newspapers feed: use HTTPS for author URL

--- core/feeds/newspapers.py
+++ core/feeds/newspapers.py
@@ -8,21 +8,23 @@ from chronam.core.models import Title
 
 class NewspaperFeedAtom(Feed):
     title = "Recent Titles from Chronicling America"
-    subtitle = "This feed lists newspaper titles that have had new content added to them."
+    subtitle = (
+        "This feed lists newspaper titles that have had new content added to them."
+    )
     feed_type = Atom1Feed
     link = "/newspapers/"
     feed_guid = "info:lc/ndnp/newspapers"
     author_name = "Library of Congress"
-    author_link = "http://loc.gov"
+    author_link = "https://www.loc.gov"
 
     def items(self):
         return Title.objects.filter(has_issues=True).annotate(last_release=Max('issues__batch__released')).order_by('-last_release')
 
     def item_link(self, item):
-        return reverse('chronam_title', args=[item.lccn])
+        return reverse("chronam_title", args=[item.lccn])
 
     def item_updateddate(self, item):
-        return item.issues.order_by('-batch__released').first().batch.released
+        return item.issues.order_by("-batch__released").first().batch.released
 
     def item_author_name(self, item):
         return item.publisher
@@ -33,4 +35,3 @@ class NewspaperFeedAtom(Feed):
 
     def item_title(self, item):
         return "Updated content for %s (%s)" % (item.name, item.place_of_publication)
-    # TODO add in thumbnail?
