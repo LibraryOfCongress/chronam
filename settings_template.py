d2bdc2fd61c0a3c7ad1338fe2abd9a814e066d8c
Chris Adams <cadams@loc.gov>
2019-03-06 15:47:59 -0500

    tidy settings

--- settings_template.py
+++ settings_template.py
@@ -1,4 +1,7 @@
+import multiprocessing
 import os
+
+import djcelery
 
 DIRNAME = os.path.abspath(os.path.dirname(__file__))
 
@@ -118,7 +121,8 @@ THUMBNAIL_WIDTH = 360
 
 DEFAULT_TTL_SECONDS = 86400  # 1 day
 LONG_TTL_SECONDS = 60 * 60 * 24 * 7 * 8  # 8 weeks
-#: Used to cache metadata about publishers, issues, etc. as distinct from HTML pages, images, search results, etc.
+#: Used to cache metadata about publishers, issues, etc. as distinct from HTML
+# pages, images, search results, etc.
 METADATA_TTL_SECONDS = DEFAULT_TTL_SECONDS
 PAGE_IMAGE_TTL_SECONDS = 60 * 60 * 24 * 7 * 2  # 2 weeks
 API_TTL_SECONDS = 60 * 60  # 1 hour
@@ -149,8 +153,6 @@ CTS_URL = "https://cts.loc.gov/transfer/"
 MAX_BATCHES = 0
 
 SENDFILE_BACKEND = "sendfile.backends.xsendfile"
-
-import multiprocessing
 
 TOO_BUSY_LOAD_AVERAGE = 1.5 * multiprocessing.cpu_count()
 # TOO_BUSY_LOAD_AVERAGE = 64
@@ -191,6 +193,5 @@ BASE_CRUMBS = [{"label": "Home", "href": "/"}]
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["*"]
 
-import djcelery
 
 djcelery.setup_loader()
