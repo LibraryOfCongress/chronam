f98baaaee7475fc0395bb8917c31753a503aa607
Chris Adams <cadams@loc.gov>
2019-07-15 16:12:28 -0400

    load_batch: validate inputs
    The loader will crash if given something which is not a fully-qualified
    filesystem path so we'll validate the value as early as possible to
    avoid confusion.

--- core/management/commands/load_batch.py
+++ core/management/commands/load_batch.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
 
 import logging
+import os
 from optparse import make_option
 
 from django.core.management.base import CommandError
 
-from chronam.core.batch_loader import BatchLoader, BatchLoaderException
+from chronam.core.batch_loader import BatchLoader
 
 from . import LoggingCommand
 
@@ -15,32 +16,37 @@ LOGGER = logging.getLogger(__name__)
 class Command(LoggingCommand):
     option_list = LoggingCommand.option_list + (
         make_option(
-            '--skip-process-ocr',
-            action='store_false',
-            dest='process_ocr',
+            "--skip-process-ocr",
+            action="store_false",
+            dest="process_ocr",
             default=True,
-            help='Do not generate ocr, and index',
+            help="Do not generate ocr, and index",
         ),
         make_option(
-            '--skip-coordinates',
-            action='store_false',
-            dest='process_coordinates',
+            "--skip-coordinates",
+            action="store_false",
+            dest="process_coordinates",
             default=True,
-            help='Do not out word coordinates',
+            help="Do not out word coordinates",
         ),
     )
     help = "Load a batch"  # NOQA: A003
-    args = '<batch name>'
+    args = "<batch path>"
 
-    def handle(self, batch_name, *args, **options):
+    def handle(self, batch_path, *args, **options):
         if len(args) != 0:
-            raise CommandError('Usage is load_batch %s' % self.args)
+            raise CommandError("Usage is load_batch %s" % self.args)
+
+        if not os.path.isdir(batch_path):
+            raise CommandError("Path %s does not exist" % batch_path)
+
+        batch_path = os.path.realpath(batch_path)
 
         loader = BatchLoader(
-            process_ocr=options['process_ocr'], process_coordinates=options['process_coordinates']
+            process_ocr=options["process_ocr"], process_coordinates=options["process_coordinates"]
         )
+
         try:
-            loader.load_batch(batch_name)
-        except BatchLoaderException:
-            LOGGER.exception("Unable to load batch %s", batch_name)
-            raise CommandError("unable to load batch. check the load_batch log for clues")
+            loader.load_batch(batch_path)
+        except Exception:
+            LOGGER.exception("Unable to load batch from %s", batch_path)
