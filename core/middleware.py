8c2a3cd847d2e857a653066d32014bd63052f643
Chris Adams <cadams@loc.gov>
2019-07-23 11:54:28 -0400

    Handle getloadavg() failures
    The glibc getloadavg() implementation parses a file under /proc and that can fail in unusual cases. This patch assumes that any system where getloadavg() fails is too busy to be useful.
    
    Resolves https://errorlogging.loc.gov/loc/ndnp/issues/1320/

--- core/middleware.py
+++ core/middleware.py
@@ -1,3 +1,5 @@
+# encoding: utf-8
+import logging
 import os
 
 from django.conf import settings
@@ -16,27 +18,43 @@ class HttpResponseServiceUnavailable(HttpResponse):
 
 
 class TooBusyMiddleware(object):
-    def process_request(self, request):
-        one, five, fifteen = os.getloadavg()
-        if one > settings.TOO_BUSY_LOAD_AVERAGE:
-            return HttpResponseServiceUnavailable(
-                """
-<!doctype html>
+    BUSY_MESSAGE = u"""
+<!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Server Too Busy - Chronicling America - Chronicling America (The Library of Congress)</title>
-  <style></style>
+  <title>Server Too Busy — Chronicling America (The Library of Congress)</title>
 </head>
 <body>
      <article>
         <h1>Server Too Busy</h1>
         <div>
-            <p>The Chronicling America server is currently too busy to serve your request. Please try your request again shortly.</p>
-            <p><a href="https://www.loc.gov/pictures/resource/ppmsc.01752/"><img src="https://memory.loc.gov/service/pnp/ppmsc/01700/01752r.jpg"/></a></p>
+            <p>
+                The Chronicling America server is currently too busy to serve your request.
+                Please try your request again shortly.
+            </p>
+            <p>
+                <a href="https://www.loc.gov/pictures/resource/ppmsc.01752/">
+                    <img src="https://memory.loc.gov/service/pnp/ppmsc/01700/01752r.jpg"/>
+                </a>
+            </p>
         </div>
     </article>
 </body>
 </html>
 """.strip()
-            )
+
+    def process_request(self, request):
+        # FIXME: this adds per-request file I/O which could be removed by
+        # calling the Linux-specific sysinfo() call directly or simply by removing
+        # it and letting Varnish handle erroring backends at that level
+
+        try:
+            one, five, fifteen = os.getloadavg()
+            too_busy = one > settings.TOO_BUSY_LOAD_AVERAGE
+        except OSError:
+            logging.exception("os.getloadavg() failed!")
+            too_busy = True
+
+        if too_busy:
+            return HttpResponseServiceUnavailable(self.BUSY_MESSAGE)
