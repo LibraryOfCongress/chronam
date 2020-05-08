3278d4a7c003992112102f5774d68e53afb77fe2
Muhibo Yusuf <myus@loc.gov>
2020-05-08 10:18:21 -0400

    Restoring poll_purge task and fixing imports and task paths

--- conf/celeryconfig.py
+++ conf/celeryconfig.py
@@ -10,7 +10,7 @@ from chronam.settings import INSTALLED_APPS
 APP_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 
-CELERY_IMPORTS = ("chronam.core.tasks",)
+CELERY_IMPORTS = ("chronam.core.tasks", "chronam.loc_cts.tasks")
 CELERYD_LOG_FILE = os.path.join("/logs/celery", "celery.log")
 CELERYD_LOG_LEVEL = logging.INFO
 CELERYD_CONCURRENCY = 2
@@ -29,10 +29,15 @@ CELERYBEAT_SCHEDULE = {
 
 if 'chronam.loc_cts' in INSTALLED_APPS:
     CELERYBEAT_SCHEDULE["poll_cts"] = {
-        "task": "chronam.core.tasks.poll_cts",
+        "task": "chronam.loc_cts.tasks.poll_cts",
         "schedule": datetime.timedelta(hours=4),
         "args": ()
-    }
+    },
+    CELERYBEAT_SCHEDULE['poll_purge'] = {
+        "task": "chronam.loc_cts.tasks.poll_purge", 
+        "schedule": crontab(hour=3, minute=0)
+    },
+    
 
 CELERYBEAT_LOG_FILE = os.path.join("/var/log/celery", "celerybeat.log")
 CELERYBEAT_LOG_LEVEL = logging.INFO
