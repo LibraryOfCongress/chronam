949aade1004b40ada874adb30ab44627d148c0ed
Chris Adams <cadams@loc.gov>
2023-01-17 20:17:19 -0500

    Avoid undefined symbol failures from flake8

--- conf/celeryconfig.py
+++ conf/celeryconfig.py
@@ -5,11 +5,12 @@ import os
 from celery.schedules import crontab
 
 from chronam.settings import *  # NOQA
+from chronam.settings import INSTALLED_APPS
 
 APP_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 
-CELERY_IMPORTS = ("chronam.core.tasks", )
+CELERY_IMPORTS = ("chronam.core.tasks",)
 CELERYD_LOG_FILE = os.path.join("/var/log/celery", "celery.log")
 CELERYD_LOG_LEVEL = logging.INFO
 CELERYD_CONCURRENCY = 2
@@ -26,18 +27,18 @@ CELERYBEAT_SCHEDULE = {
     },
 }
 
-if 'chronam.loc_cts' in INSTALLED_APPS:
-    CELERY_IMPORTS += ("chronam.loc_cts.tasks", )
+if "chronam.loc_cts" in INSTALLED_APPS:
+    CELERY_IMPORTS += ("chronam.loc_cts.tasks",)
     CELERYBEAT_SCHEDULE["poll_cts"] = {
         "task": "chronam.loc_cts.tasks.poll_cts",
         "schedule": datetime.timedelta(hours=1),
-        "args": ()
+        "args": (),
     }
 
     CELERYBEAT_SCHEDULE["poll_purge"] = {
         "task": "chronam.loc_cts.tasks.poll_purge",
         "schedule": crontab(hour=3, minute=0),
-        "args": ()
+        "args": (),
     }
 
 
